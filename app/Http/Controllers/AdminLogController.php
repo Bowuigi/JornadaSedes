ea75d3a117b97685b361cf9dae144b30f930be5c
jeremias <jeremiasmaiasd@gmail.com>
2026-05-20 17:19:50 -0300

    controlador y rutas agregados

--- app/Http/Controllers/AdminLogController.php
+++ app/Http/Controllers/AdminLogController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AdminLogController extends Controller
 {
-    //
+    public function index (){
+
+    return view ('adminlogin');
+
+    } 
 }
