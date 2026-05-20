20e7fafcdd72262821747485154b1e17ede68aed
jeremias <jeremiasmaiasd@gmail.com>
2026-05-20 18:14:28 -0300

    rutas colocadas

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/inscription', [App\Http\Controllers\InscriptionController::class, 'index']);
+Route::post('/inscription', [App\Http\Controllers\InscriptionController::class, 'store']);
 
 require __DIR__.'/auth.php';
